6f03c95d0ea6994da4c220ed66d3851ce97348c5
septa97 <jancharles.adona@gmail.com>
2019-04-12 16:47:40 +0800

    Add day 2 part 2. Rename day 2 part1.txt to input.txt

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -3,11 +3,22 @@ import scala.io.Source
 object Day2 {
   def main(args: Array[String]): Unit = {
     val counts = for {
-      line <- Source.fromFile("input/day2/part1.txt").getLines().toList
+      line <- Source.fromFile("input/day2/input.txt").getLines().toList
       charCount = line.groupBy(identity).mapValues(_.length)
     } yield charCount.exists(_._2 == 2) -> charCount.exists(_._2 == 3)
 
     println(
-      s"${counts.map(_._1).count(identity) * counts.map(_._2).count(identity)}")
+      s"part 1: ${counts.map(_._1).count(identity) * counts.map(_._2).count(identity)}")
+
+    val lines = Source.fromFile("input/day2/input.txt").getLines().toList
+    val diffs = for {
+      i <- lines.indices
+      j <- i + 1 until lines.length
+      zipped = lines(i).zip(lines(j))
+    } yield
+      zipped.filter(e => e._1 == e._2).map(_._1) -> zipped.count(e =>
+        e._1 != e._2)
+
+    println(s"part 2: ${diffs.minBy(_._2)._1.mkString}")
   }
 }
